0d9ff0b1ab195691acb8e27cf3eb78e8336cbaae
SLieng <lieng.simon96@gmail.com>
2018-02-08 20:08:56 +0000

    new input

--- js/input.js
+++ js/input.js
@@ -25,3 +25,35 @@ document.addEventListener('keyup', function(event) {
 	  break;
 	}
 });
+
+Game.prototype.readKeyboardInput = function () {
+    //PLAYER 1
+	if (keysDown.player1Left) {
+		this.paddle1.cmdMoveLeft();
+	} else if (keysDown.player1Right) {
+		this.paddle1.cmdMoveRight();
+	} else {
+		this.paddle1.cmdStop();
+	}
+}
+
+Game.prototype.readAIInput = function () {
+	let numCommands = this.ai.length;
+	let timePassed = this.timePassed;
+	let receivedCmd = false;
+
+	for(let i=1; i<numCommands; i++) {
+		if(this.ai[i].timePassed > timePassed) {
+			this.paddle2.receiveCmd(this.ai[i-1].cmd);
+			receivedCmd = true;
+			break;
+		}
+	}
+	
+	if(!receivedCmd) {
+		//console.log(this.ai[numCommands-1].cmd);
+			this.paddle2.receiveCmd(this.ai[numCommands-1].cmd);
+	}
+
+	//console.log(this.paddle2.pos.x);
+}
--- js/pong.js
+++ js/pong.js
@@ -213,38 +213,3 @@ Game.prototype.begin = function () {
 Game.prototype.spawnBall = function(ball) {
     this.actors.push(ball);
 }
-
-////////////////////////////////////// Input
-
-Game.prototype.readKeyboardInput = function () {
-    //PLAYER 1
-	if (keysDown.player1Left) {
-		this.paddle1.cmdMoveLeft();
-	} else if (keysDown.player1Right) {
-		this.paddle1.cmdMoveRight();
-	} else {
-		this.paddle1.cmdStop();
-	}
-}
-
-Game.prototype.readAIInput = function () {
-	let numCommands = this.ai.length;
-	let timePassed = this.timePassed;
-	let receivedCmd = false;
-
-	for(let i=1; i<numCommands; i++) {
-		if(this.ai[i].timePassed > timePassed) {
-			this.paddle2.receiveCmd(this.ai[i-1].cmd);
-			receivedCmd = true;
-			break;
-		}
-	}
-	
-	if(!receivedCmd) {
-		console.log("DKLSFJFDLK");
-		console.log(this.ai[numCommands-1].cmd);
-			this.paddle2.receiveCmd(this.ai[numCommands-1].cmd);
-	}
-
-	console.log(this.paddle2.pos.x);
-}
